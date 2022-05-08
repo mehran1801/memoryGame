36680847c5bb0cc5e236e3d6e968cfba297cff46
Mehran <mehran1801@gamil.com>
2022-05-08 22:35:24 +1000

    images folder added

---
+++ main.js
@@ -0,0 +1,58 @@
+const cardArray = [
+    {
+    name: 'fries',
+    img: 'images/fries.png',
+},
+    {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+},
+    {
+    name: 'fries',
+    img: 'images/fries.png',
+},
+    {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+},
+    {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+},
+    {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+},
+    {
+    name: 'pizza',
+    img: 'images/pizza.png',
+},
+{
+    name: 'fries',
+    img: 'images/fries.png',
+},
+    {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png',
+},
+    {
+    name: 'fries',
+    img: 'images/fries.png',
+},
+    {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+},
+    {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png',
+},
+    {
+    name: 'milkshake',
+    img: 'images/milkshake.png',
+},
+    {
+    name: 'pizza',
+    img: 'images/pizza.png',
+},
+]
